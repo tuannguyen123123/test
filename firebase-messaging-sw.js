43af9db95a68d08339a5bb7fd419c932cc12755d
tuannguyen123123 <tuannd04424@gmail.com>
2021-08-25 20:11:03 +0700

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,44 @@
+importScripts("https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+var firebaseConfig = {
+    apiKey: "AIzaSyBiuBam_SZ_LOTW5QLOmRrO7XdkWRbhDP8",
+    authDomain: "cmc2021-f4f8b.firebaseapp.com",
+    projectId: "cmc2021-f4f8b",
+    storageBucket: "cmc2021-f4f8b.appspot.com",
+    messagingSenderId: "364371829051",
+    appId: "1:364371829051:web:7375833599efa45b9c971e",
+    measurementId: "G-73KBH7SW8L"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+
+ messaging.setBackgroundMessageHandler(function(payload) {
+     console.log(payload);
+
+ });
+
+// messaging.setBackgroundMessageHandler(function(payload) {
+//     console.log(
+//         "[firebase-messaging-sw.js] Received background message ",
+//         payload,
+//     );
+//     // Customize notification here
+//     const notificationTitle = "Background Message Title";
+//     const notificationOptions = {
+//         body: "Background Message body.",
+//         icon: "/itwonders-web-logo.png",
+//     };
+
+//     return self.registration.showNotification(
+//         notificationTitle,
+//         notificationOptions,
+//     );
+// });
